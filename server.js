b68996d4a8f304c4b94fc6ec17efb73d6414d661
SamiEddin <sae8011@gmail.com>
2025-03-18 23:49:44 +0000

    Fix API route name

--- server.js
+++ server.js
@@ -21,3 +21,12 @@ console.log("✅ Middleware applied successfully");
 
 const PORT = process.env.PORT || 10000;
 app.listen(PORT, "0.0.0.0", () => console.log(`✅ Server running on port ${PORT}`));
+app.post("/create_connection_token", authenticate, async (req, res) => {
+    try {
+      const token = await stripe.terminal.connectionTokens.create();
+      res.json({ secret: token.secret });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
+  
